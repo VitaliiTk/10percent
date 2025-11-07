ce3a06130074741b75864463dacee243fcd9509c
VitaliiTk <tka4uk.gt41@gmail.com>
2025-11-07 16:49:50 +0600

    commit html

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (_, res) => {
-  res.send({ message: 'Express server is run' })
+  res.send('<h1>Server is run</h1>')
 })
 
 app.listen(3000, () => console.log('Server is running on port 3000'))
