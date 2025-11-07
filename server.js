eb69405ef22e6f4397d2e7931e0dd29ba8f42529
VitaliiTk <tka4uk.gt41@gmail.com>
2025-11-07 17:52:14 +0600

    port new

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-const PORT = 8080
+const PORT = 80
 
 // Указываем, что движок шаблонов — ejs
 app.set('view engine', 'ejs')
