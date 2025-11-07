49e831629108c684ff850fbb14989890c7aeb33d
VitaliiTk <tka4uk.gt41@gmail.com>
2025-11-07 17:12:59 +0600

    commit

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-import express from 'express'
+const express = require('express')
 const app = express()
+
+const PORT = 3000
+
+// Указываем, что движок шаблонов — ejs
+app.set('view engine', 'ejs')
 
 app.get('/', (_, res) => {
   res.send('<h1>MotoBoard test server is running</h1>')
 })
 
-app.listen(3000, () => console.log('Server is running on port 3000'))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
