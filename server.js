63eae60208b523b5786a54e0763f1d3b48235b67
VitaliiTk <tka4uk.gt41@gmail.com>
2025-11-07 16:56:44 +0600

    motoboard

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (_, res) => {
-  res.send('<h1>Server is run</h1>')
+  res.send('<h1>MotoBoard test server is running</h1>')
 })
 
 app.listen(3000, () => console.log('Server is running on port 3000'))
