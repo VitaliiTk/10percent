6d16519bb7ad5fdb47ebe6cae472fd44aba8b330
VitaliiTk <tka4uk.gt41@gmail.com>
2025-11-07 14:27:55 +0600

    second comiy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (_, res) => {
-  res.send({ message: 'Hello from Express server' })
+  res.send({ message: 'Express server' })
 })
 
 app.listen(3000, () => console.log('Server is running on port 3000'))
